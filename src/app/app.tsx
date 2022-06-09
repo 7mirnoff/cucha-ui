99d3f2583e758fdb234954a995cea34322f41d66
Dmitry Smirnov <sdmitry3@PMINTL.NET>
2022-06-09 15:11:58 +0300

    add three js and scss npm

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
+import { Canvas } from '@react-three/fiber'
+
+import styles from './app.module.scss'
+
 export const App: React.FC = () => {
-  return <div>sdfsf333s</div>
+  return (
+    <div className={styles.scene}>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color='red' position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  )
 }
